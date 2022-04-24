c424c20824ed36657396dc94235a451a500e15e3
1-Accelerator-1 <max.pashuta@mail.ru>
2022-04-24 16:48:33 +0300

    Refactered ConvertNumberToString method for new unit test.

--- Task_7/FizzBuzz.cs
+++ Task_7/FizzBuzz.cs
@@ -1,9 +1,16 @@
-﻿namespace Task_7
+﻿using Task_7.ValidationExceptions;
+
+namespace Task_7
 {
     internal class FizzBuzz : IFizzBuzz
     {
         public string ConvertNumberToString(int number)
         {
+            if (number < 1 || number > 100)
+            {
+                throw new NumberOutOfRangeException("The number out of range (1 - 100).");
+            }
+
             return number.ToString();
         }
     }
