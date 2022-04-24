36421ea62b2a6c6547aefb9cd1bc41b388f67473
1-Accelerator-1 <max.pashuta@mail.ru>
2022-04-24 16:21:58 +0300

    Added first unit test.

---
+++ Task_7.UnitTests/FizzBuzzUnitTests.cs
@@ -0,0 +1,30 @@
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Task_7.UnitTests
+{
+    [TestFixture]
+    public class FizzBuzzUnitTests
+    {
+        private readonly IFizzBuzz _fizzBuzz;
+
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [TestCase(1)]
+        [TestCase(4)]
+        [TestCase(13)]
+        public void ConvertNumberToString_PassNumberFrom1To100WithoutNumbersMultiplesOfThreeAndFive_ShouldReturnStringNumber(int number)
+        {
+            // Act
+            var stringNumber = _fizzBuzz.ConvertNumberToString(number);
+
+            // Assert
+            stringNumber.Should().NotBeNullOrEmpty();
+            stringNumber.Should().BeEquivalentTo(number.ToString());
+        }
+    }
+}
