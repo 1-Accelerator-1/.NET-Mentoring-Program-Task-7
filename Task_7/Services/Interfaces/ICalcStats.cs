89c6f96e8faa7510e0c70dc444abecac949437e5
1-Accelerator-1 <max.pashuta@mail.ru>
2022-04-24 17:17:00 +0300

    Created new ICalcStats interface for new service.

---
+++ Task_7/Services/Interfaces/ICalcStats.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Task_7.Services.Interfaces
+{
+    public interface ICalcStats
+    {
+        int MaxNumber { get; }
+
+        int MinNumber { get; }
+
+        int NumberOfElements { get; }
+
+        int Average { get; }
+
+        Task CalcCollectionStats(IEnumerable<int> collection);
+    }
+}
