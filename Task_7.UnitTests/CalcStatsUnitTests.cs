f72d5f77921c126c06bc20471f9d49b06c4de08e
1-Accelerator-1 <max.pashuta@mail.ru>
2022-04-24 17:40:04 +0300

    Added second unit test for CalcStats service.

--- Task_7.UnitTests/CalcStatsUnitTests.cs
+++ Task_7.UnitTests/CalcStatsUnitTests.cs
@@ -1,5 +1,6 @@
 ﻿using FluentAssertions;
 using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Task_7.Services;
@@ -32,6 +33,19 @@ namespace Task_7.UnitTests
             _calcStats.MinNumber.Should().Be(-5);
             _calcStats.NumberOfElements.Should().Be(7);
             _calcStats.Average.Should().BeApproximately(2.857, 0.001);
+        }
+
+        [Test]
+        public async Task CalcCollectionStats_PassNull_ShouldThrowArgumentNullException()
+        {
+            // Arrange
+            List<int> testCollection = null;
+
+            // Act
+            Func<Task> testFunc = async () => await _calcStats.CalcCollectionStats(testCollection);
+
+            // Assert
+            await testFunc.Should().ThrowAsync<ArgumentNullException>();
         }
     }
 }
