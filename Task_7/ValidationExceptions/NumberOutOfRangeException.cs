84dfab649759ecacd4f3b9bb6c8dc30101d19322
1-Accelerator-1 <max.pashuta@mail.ru>
2022-04-24 16:45:32 +0300

    Added new custom exception. It is NumberOutOfRangeException.

---
+++ Task_7/ValidationExceptions/NumberOutOfRangeException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Task_7.ValidationExceptions
+{
+    public class NumberOutOfRangeException : Exception
+    {
+        public NumberOutOfRangeException()
+        {
+        }
+
+        public NumberOutOfRangeException(string message)
+            : base(message)
+        {
+        }
+
+        public NumberOutOfRangeException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}
